8f23a19dbdceeca3bc2d9eaf7b0e7919494ac488
Amelia Davis <ameliadavis271@gmail.com>
2020-11-06 18:24:59 -0800

    Devise User implemented with username method, relevant sign in buttons etc made

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -13,7 +13,7 @@ def new
 end
 
 def create
-  @listing = Listing.new(listing_params)
+  @listing = current_user.listings.new(listing_params)
   if @listing.save
     redirect_to listings_path
   else 
