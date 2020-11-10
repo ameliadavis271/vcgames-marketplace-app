e4f73ebb7235d237b2837937f78f5a44a36f432f
Amelia Davis <ameliadavis271@gmail.com>
2020-11-10 03:20:20 -0800

    added methods in profiles, may or may not need editing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,34 @@
 class ProfilesController < ApplicationController
-
+  before_action :find_profile, only: [:show, :edit, :update, :destroy]  
+  before_action :authenticate_user!, except: [:show]
+  
+  def index
+    @profile = Profile
+  end
+  
   def show
+    @profile = Profile.find(profile_params)
+  end
+  
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+  
+  def update
+    @profile = Profile.find(params[:id])
+    @profile.update(profile_params)
+    redirect_to profiles_path
+  end
+  
+  private
+  
+  def profile_params
+    params.require(:listing).permit(:fullname, :description, :avatar)
+  end
+  
+  def find_profile
     @profile = Profile.find(params[:id])
   end
 
-  def new
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
 end
+  
