23a9a830ce30e48789fe3422e97984348e716e15
Amelia Davis <ameliadavis271@gmail.com>
2020-11-11 22:02:31 -0800

    All profile pages viewable and working well. Also forced to create profile after signup

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :fullname, presence: true
+  validates :description, presence: true
+  validates :user_id, presence: true
 
   has_one_attached :avatar
 end
