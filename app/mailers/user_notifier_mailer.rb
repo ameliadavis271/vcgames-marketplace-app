74f88f93496bf396c348580be5a03d2ff658eca6
Amelia Davis <ameliadavis271@gmail.com>
2020-11-15 02:21:41 -0800

    verified sender auth

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,4 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
+default from: 'ameliadavis271@gmail.com'
 
   def send_signup_email(user)
     mail( to: @user.email,subject: 'Thanks for signing up for our amazing app' )
