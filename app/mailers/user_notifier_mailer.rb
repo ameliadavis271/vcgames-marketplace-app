baccdc89130c1f6d158b303388d22b93d16115c8
Amelia Davis <ameliadavis271@gmail.com>
2020-11-15 16:49:42 -0800

    Mailer Test Heroku

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -5,7 +5,7 @@ default from: 'ameliadavis271@gmail.com'
     mail( to: @user.email,subject: 'Thanks for signing up for our amazing app' )
   end
 
-  def send_listing_new_mail(user)
+  def send_listing_new_mail(user, listing)
     @user = user
     mail( to: @user.email, subject: 'Thanks for creating a new listing!' )
   end
