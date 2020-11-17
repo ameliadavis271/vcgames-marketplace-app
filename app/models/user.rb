a19d4c95c4c6081cea1a8dc46afa4503f72d56f4
Amelia Davis <ameliadavis271@gmail.com>
2020-11-17 01:26:46 -0800

    forcing user to create profile and new background

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true
+  validates :email, presence: true
+
   has_many :listings
   has_one :address
   has_one :profile, dependent: :destroy
