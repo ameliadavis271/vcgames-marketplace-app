b0ab243984ab46a8b49f9729e9b592fa94aa5538
Amelia Davis <ameliadavis271@gmail.com>
2020-11-16 22:36:13 -0800

    Merge2?

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,7 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
 
-<<<<<<< HEAD
-  has_many :authored_cnversations, class_name: 'Conversation', foreign_key: 'author_id'
-  has_many :received_cnversations, class_name: 'Conversation', foreign_key: 'receiver_id'
-  has_many :personal_messages, dependent: :destroy
-=======
 has_many :authored_conversations, -> { order(updated_at: :desc)}, dependent: :destroy, class_name: 'Conversation', foreign_key: 'author_id'
 has_many :received_conversations, -> { order(updated_at: :desc)}, dependent: :destroy, class_name: 'Conversation', foreign_key: 'receiver_id'
 has_many :personal_messages, dependent: :destroy
->>>>>>> 2ac50e61ab1c705033e72a13b9e0c0eaa2311b06
 end
